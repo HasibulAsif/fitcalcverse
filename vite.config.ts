23f97dd7c30189a362836a3ed94a8ebb389d70e9
HasibulAsif <80703827+HasibulAsif@users.noreply.github.com>
2024-12-17 06:06:16 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/workout.healthythako.com/" : "/", // Ensures correct base URL
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
